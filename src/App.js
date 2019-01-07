249c31179717d9b1cda235a21ebe3507b9310dc1
Kishan Patel <kishan02patel@gmail.com>
2019-01-08 00:14:02 +0530

    Added comments to the code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import DisplayPost from './DisplayPost';
 class App extends Component {
 	constructor() {
 		super()
+		//Reference is created to get the reference of div tag.
 		this.myDivRef = React.createRef()
+
+		// pageSize can be changed according to the needs
 		this.state = {
 			posts: [],
 			offset: 0,
@@ -14,16 +17,24 @@ class App extends Component {
 	}
 
 	componentDidMount() {
+		// Fetch the data from the server.
 		fetch('http://jsonplaceholder.typicode.com/posts')
 			.then(response => response.json())
 			.then(data => this.setState({
 				posts: data
 			}))
 
+		// Add a scroll event listener.
 		window.addEventListener('scroll', () => {
+			// Get the height of div tag.
 			let windowHeight = this.myDivRef.current.clientHeight
+
+			// Get the offset till where user has scrolled.
 			let scrollOffset = window.pageYOffset
+
+			//If the scroll percentage is 70% then increase the offset value by 20(Can be changed according to needs.)
 			if (scrollOffset / windowHeight > 0.7) {
+				// Check if sum of pageSize and offset is less than the length of array
 				if (this.state.offset + this.state.pageSize <= this.state.posts.length) {
 					this.setState(
 						prevState => ({ offset: prevState.offset + 20 }),
@@ -34,21 +45,35 @@ class App extends Component {
 		})
 	}
 
+	// Endless paging
 	paging() {
-		let array = [], post
+		// displayArray is used to push the div tags which will be returned to render method
+		let displayArray = [], post
+
+		/*
+		** Check if the sum of pageSize and offset is greater than the array length. 
+		** If true then loop only till the array length
+		** Else loop till the sum of pageSize and offset
+	   	*/
 		let loopLength = this.state.pageSize + this.state.offset >= this.state.posts.length ? this.state.posts.length : this.state.pageSize + this.state.offset
+
 		for (let i = 0; i < loopLength; i++) {
+			// Get the ith post 
 			post = this.state.posts[i];
-			array.push(< DisplayPost key={post.id + post.userId} post={post} index={i} />)
+			// Push the div tag return by DisplayPost into array.
+			displayArray.push(< DisplayPost key={post.id + post.userId} post={post} index={i} />)
 		}
 
-		return array
+		// Return the array to render method for displaying.
+		return displayArray
 	}
 
 	render() {
 		return (
+			// Create a reference of div tag so that its height can be calculated.
 			<div className="App" ref={this.myDivRef}>
 				{
+					// Call paging function only if there is data. Wait for data to be fetched from server.
 					this.state.posts.length <= 0 ? '' : this.paging()
 				}
 			</div>
--- src/DisplayPost.jsx
+++ src/DisplayPost.jsx
@@ -4,14 +4,6 @@ class DisplayPost extends React.Component {
 	render() {
 		return (
 			<div class="post">
-				{
-					/*
-					<h3>{this.props.index} - {this.props.post.title}</h3>
-					<h4>By ~ {this.props.post.userId}</h4>
-					<p>{this.props.post.body}</p>
-					<br />
-					*/
-				}
 				<div class="card text-white bg-dark mb-3" style={{ maxWidth: "18rem;" }}>
 					<div class="card-header">Post Number - {this.props.index + 1} <br /> ~ By UserId:  {this.props.post.userId}</div>
 					<div class="card-body">
